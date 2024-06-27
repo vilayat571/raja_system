375311fa2c12a54a7f44f91add9a8d9ee29d09e6
Vilayat Safarov <vilayat571@gmail.com>
2024-06-27 11:48:12 -0700

    font customization

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface ILayout{
 }
 const Layout:React.FC<ILayout>= ({children}) => {
   return (
-    <div>
+    <div className='overflow-x-hidden'>
       {children}
       <Foot />
       <Totop />
