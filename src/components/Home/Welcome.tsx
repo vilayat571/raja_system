83955e047175abe3cded3a6730f26d63a2f52348
Vilayat Safarov <vilayat571@gmail.com>
2024-06-27 12:24:50 -0700

    font size testing

--- src/components/Home/Welcome.tsx
+++ src/components/Home/Welcome.tsx
@@ -1,7 +1,7 @@
 function Welcome() {
   return (
     <div
-      className="text-center px-2 relative xl:top-28 lg:top-28 sm:top-40 leading-tight font-semibold text-white 
+      className="text-center px-2 relative xl:top-28 lg:top-28 sm:top-44 leading-tight font-semibold text-white 
 xl:text-[5vw] lg:text-[4vw] sm:text-[8vw] md:text-[5vw] tracking-wide"
     >
       Welcome
@@ -10,7 +10,9 @@ xl:text-[5vw] lg:text-[4vw] sm:text-[8vw] md:text-[5vw] tracking-wide"
       <br className="xl:block md:block lg:block sm:hidden" />
       Restaurant
       <p className=" font-thin text-[3vh] mt-4">
-        <button className="xl:px-6 lg:px-6 md:px-5 sm:px-4 py-3 border-white border-[1px] rounded xl:text-normal lg:text-normalsm:text-[16px] md:text-normal tracking-wider mt-1">
+        <button className="xl:px-6 lg:px-6 md:px-5 sm:px-4 py-3 border-white border-[1px] rounded 
+        xl:text-normal lg:text-normal sm:text-[16px] md:text-normal 
+        tracking-wider mt-1">
           See menu
         </button>
       </p>
